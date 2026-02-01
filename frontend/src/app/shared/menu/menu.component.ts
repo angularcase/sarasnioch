27f182db01d028d404c7207ed853b7f9f715780f
tomasz.dobosz <tomasz.dobosz@tiskel.com>
2026-02-01 21:40:29 +0100

    menu

--- frontend/src/app/shared/menu/menu.component.ts
+++ frontend/src/app/shared/menu/menu.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
-  selector: 'app-menu',
-  standalone: true,
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+    selector: 'app-menu',
+    standalone: true,
+    imports: [CommonModule, AvatarModule, StyleClassModule],
+    templateUrl: './menu.component.html',
+    styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+    navs = signal([
+        {
+            label: 'Home',
+            icon: 'pi-home',
+            to: ''
+        },
+        {
+            label: 'Artykuly',
+            icon: 'pi-book',
+            to: ''
+        },
+        {
+            label: 'Produkty',
+            icon: 'pi-briefcase',
+            to: ''
+        },
+        {
+            label: 'Kontakt',
+            icon: 'pi-envelope',
+            to: ''
+        }
+    ]);
+
+    selectedNav: string = 'Home';
 }
