c21ba53e551f63d2779f45b6b193c0a3768d25fb
Tomasz Dobosz <angular.case@gmail.com>
2026-02-21 10:57:49 +0100

    poprwaki

--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -1,20 +1,38 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from '@strapi/strapi';
+
+const PUBLIC_PERMISSIONS = [
+  { action: 'api::product.product.find' },
+  { action: 'api::product.product.findOne' },
+  { action: 'api::manufacturer.manufacturer.find' },
+  { action: 'api::manufacturer.manufacturer.findOne' },
+  { action: 'api::article.article.find' },
+  { action: 'api::article.article.findOne' },
+  { action: 'api::animal-category.animal-category.find' },
+  { action: 'api::animal-category.animal-category.findOne' },
+];
 
 export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
   register(/* { strapi }: { strapi: Core.Strapi } */) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    const publicRole = await strapi
+      .query('plugin::users-permissions.role')
+      .findOne({ where: { type: 'public' } });
+
+    if (!publicRole) return;
+
+    const existingPermissions = await strapi
+      .query('plugin::users-permissions.permission')
+      .findMany({ where: { role: publicRole.id } });
+
+    const existingActions = new Set(existingPermissions.map((p) => p.action));
+
+    for (const perm of PUBLIC_PERMISSIONS) {
+      if (!existingActions.has(perm.action)) {
+        await strapi.query('plugin::users-permissions.permission').create({
+          data: { action: perm.action, role: publicRole.id },
+        });
+      }
+    }
+  },
 };
